efc16c2432261799e1d77e27725cec9db26a5caf
addyTya <adityarajdonbosco@gamil.com>
2024-10-21 04:09:59 +0530

    fixed port issue

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/revealpassword', async (req, res) => {
 });
 
 // Use dynamic port for production (Vercel) and fallback to 3001 for local development
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
